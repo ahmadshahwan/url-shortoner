f2cb8f3156da893966710725577115b2534802a4
Ahmad Shahwan <ahmad.shahwan@gmail.com>
2023-07-21 15:32:12 +0200

    Update DTO and JSON settings

--- src/main/java/fr/sncf/d2d/web/shortener/api/controllers/dto/AliasedLinkResponse.java
+++ src/main/java/fr/sncf/d2d/web/shortener/api/controllers/dto/AliasedLinkResponse.java
@@ -2,14 +2,16 @@ package fr.sncf.d2d.web.shortener.api.controllers.dto;
 
 import fr.sncf.d2d.web.shortener.domain.AliasedLink;
 
+import java.net.URL;
 import java.util.UUID;
 
 public record AliasedLinkResponse(
         UUID id,
-        String shortUrl
+        String shortId,
+        URL realUrl
 ) {
 
     public static AliasedLinkResponse from(AliasedLink aliasedLink) {
-        return new AliasedLinkResponse(aliasedLink.id(), aliasedLink.alias());
+        return new AliasedLinkResponse(aliasedLink.id(), aliasedLink.alias(), aliasedLink.url());
     }
 }
